b265100eb2cfbb1fd2bbdd4da470bb5586efc66d
Narayan Subedi <prabhat.aurora@gmail.com>
2015-11-29 22:30:56 +0545

    Some clean up and added blog page

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
-
-	$(function(){
-		var year = new Date().getFullYear();
-		var copyRight = '&copy; Copyright '+year+'. All Right Reserved.';
-		document.getElementById("footer-text").innerHTML=copyRight;
-	});	
+$(function(){
+	var year = new Date().getFullYear();
+	var copyRight = '&copy; Copyright '+year+'. All Right Reserved.';
+	document.getElementById("footer-text").innerHTML=copyRight;
+});	
+function highlightCurrentPage(id){
+	$('.active').removeClass('active');
+	$('#'+id).addClass('active');
+}
