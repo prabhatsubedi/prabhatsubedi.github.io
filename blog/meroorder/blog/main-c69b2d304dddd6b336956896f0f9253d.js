4223c2121c4f154b104add5ef325e0f2fc00a856
Prabhat Subedi <prabhat.aurora@gmail.com>
2023-08-21 15:25:54 +0545

    blog

---
+++ blog/meroorder/blog/main-c69b2d304dddd6b336956896f0f9253d.js
@@ -0,0 +1,15 @@
+var process = process || {env: {NODE_ENV: "development"}};
+(function () {
+    window.onscroll = function () {
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".scroll-top");
+        console.log(backToTo)
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+}) ();
+
